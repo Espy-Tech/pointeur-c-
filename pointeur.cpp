c810cd63870e42d48d151340047127a714b5cee2
Espy <ibrahimsamf23@gmail.com>
2026-02-07 21:19:39 +0300

    first repository

---
+++ pointeur.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "SIZE: ";
+    cin >> size;
+
+    int *tab = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "A[" << i << "] : ";
+        cin >> tab[i];
+    }
+
+    cout << "Hello World";
+    return 0;
+}
